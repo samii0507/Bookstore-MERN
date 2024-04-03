33ed18c39678b017c6f22c474319fae255f457ef
Samitha <samitha.nillupul1440@gmail.com>
2024-04-03 20:55:56 +0530

    contact schema

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express();
-const mongoose = require('mongoose')
-
+//const mongoose = require('mongoose')
+connectDb();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-mongoose.connect('mongodb+srv://root:root1234@samicluster.2cipebb.mongodb.net/?retryWrites=true&w=majority&appName=samiCluster');
+//mongoose.connect('mongodb+srv://root:root1234@samicluster.2cipebb.mongodb.net/?retryWrites=true&w=majority&appName=samiCluster');
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
